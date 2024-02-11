16cfe8e116e5e4ef66b449b9af1e663fc620b84c
Elad <elel290603@gmail.com>
2024-02-11 21:29:10 +0200

    BFS G

--- Algorithms.py
+++ Algorithms.py
@@ -5,12 +5,58 @@ from typing import List, Tuple
 import heapdict
 
 
+# class node():
+#     def __init__(self, state, father=None):
+#         self.state = state
+#         self.father = father
+#         self.children = []
+#
+#     def expand(self, env):
+#         print(env.succ(self.state).values())
+#         for (state, cost, terminated) in env.succ(self.state).values():
+#             if (cost != np.inf):
+#                 self.children.append((state, cost, terminated))
+
+
 class BFSAgent():
+
     def __init__(self) -> None:
         pass
 
     def search(self, env: DragonBallEnv) -> Tuple[List[int], float, int]:
-        raise NotImplementedError
+        close = []
+        curr = env.get_state()
+
+        open = [curr]
+        fathers = {curr: (None, -1)}
+        costs = {curr: 0}
+
+        while not len(open) == 0:
+            curr = open[0]
+            open = open[1:]
+            close.append(curr)
+
+            for action, ((position, _, _), cost, _) in env.succ(curr).items():
+                if cost != np.inf:
+                    state = (position, curr[1] or (position == env.d1[0]), curr[2] or (position == env.d2[0]))
+
+                    if (state not in close) and (state not in open):
+                        if env.is_final_state(state):
+                            #Calculate track to the solution
+                            actions = [action]
+                            iterator = curr
+                            while fathers[iterator][1] != -1:
+                                actions = [fathers[iterator][1]] + actions
+                                iterator = fathers[iterator][0]
+
+                            return actions, costs[curr] + cost, len(close)
+
+                        open.append(state)
+                        fathers[state] = (curr, action)
+                        costs[state] = costs[curr] + cost
+                        print(state)
+
+        return [], -1, -1
 
 
 class WeightedAStarAgent():
@@ -21,10 +67,9 @@ class WeightedAStarAgent():
         raise NotImplementedError
 
 
-
 class AStarEpsilonAgent():
     def __init__(self) -> None:
         raise NotImplementedError
-        
+
     def ssearch(self, env: DragonBallEnv, epsilon: int) -> Tuple[List[int], float, int]:
         raise NotImplementedError
--- main.py
+++ main.py
@@ -21,9 +21,39 @@ MAPS = {
     ],
 }
 
+
+def print_solution(actions, env: DragonBallEnv) -> None:
+    env.reset()
+    total_cost = 0
+    print(env.render())
+    print(f"Timestep: {1}")
+    print(f"State: {env.get_state()}")
+    print(f"Action: {None}")
+    print(f"Cost: {0}")
+    time.sleep(1)
+
+    for i, action in enumerate(actions):
+        state, cost, terminated = env.step(action)
+        total_cost += cost
+        clear_output(wait=True)
+
+        print(env.render())
+        print(f"Timestep: {i + 2}")
+        print(f"State: {state}")
+        print(f"Action: {action}")
+        print(f"Cost: {cost}")
+        print(f"Total cost: {total_cost}")
+
+        time.sleep(1)
+
+        if terminated is True:
+            break
+
+#Main Test
 env = DragonBallEnv(MAPS["8x8"])
 
 env.reset()
+
 BFS_agent = BFSAgent()
 actions, total_cost, expanded = BFS_agent.search(env)
 print(f"Total_cost: {total_cost}")
@@ -31,3 +61,6 @@ print(f"Expanded: {expanded}")
 print(f"Actions: {actions}")
 
 assert total_cost == 119.0, "Error in total cost returned"
+
+print_solution(actions, env)
+
